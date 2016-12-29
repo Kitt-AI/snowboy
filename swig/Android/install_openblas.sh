89c8788d96ec969d3fa4c5ef8f0f8cd171bdb72b
chenguoguo <chenguoguo06@gmail.com>
2016-12-29 22:49:05 +0000

    Improvements to the Android swig compilation part

--- swig/Android/install_openblas.sh
+++ swig/Android/install_openblas.sh
@@ -4,18 +4,18 @@
 # prebuild Android NDK toolchains do not include Fortran, hence parts like
 # LAPACK will not be built.
 
-ARCH=$1
-TARGET=$2
+CC=$1
+AR=$2
+TARGET=$3
 
 if [ ! -f OpenBLAS-0.2.18.tar.gz ]; then
   wget -T 10 -t 3 https://codeload.github.com/xianyi/OpenBLAS/tar.gz/v0.2.18 \
   -O OpenBLAS-0.2.18.tar.gz || exit 1;
 fi
 
-tar -xvzf OpenBLAS-0.2.18.tar.gz || exit 1;
+tar -xvzf OpenBLAS-0.2.18.tar.gz 1>/dev/null || exit 1;
 mv OpenBLAS-0.2.18 OpenBLAS-Android
 
 cd OpenBLAS-Android
-make TARGET=${TARGET} HOSTCC=gcc \
-  CC=${ARCH}-linux-androideabi-gcc NOFORTRAN=1 || exit 1;
+make TARGET=${TARGET} HOSTCC=gcc CC=${CC} AR=${AR} NOFORTRAN=1 || exit 1;
 make PREFIX=`pwd`/install install || exit 1;
