85484b0b438c5944cd0535313e500080189a475c
Igor Boguslavsky <iboguslavsky@gmail.com>
2017-01-24 12:55:45 -0500

    Modify script description header

--- examples/Perl/googlevoice.pl
+++ examples/Perl/googlevoice.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 #
-# Uses snowboy to wake up and then collect audio to send to Google Speech API for further recognition
-# Takes Personal Model as a first argument
+# This example uses Snowboy to wake up and then collect audio to send to Google Speech API for further recognition. It works with both personal and universal models. Please set your Google API key below in order to use it.
 
 use lib 'Snowboy';
 
