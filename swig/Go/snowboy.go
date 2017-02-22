73c722c750ad605020efcfdfd3d930ee05bae845
Brent Dimmig <brentdimmig@gmail.com>
2017-02-22 15:33:24 -0500

    Add Go bindings for snowboy with swigcxx

---
+++ swig/Go/snowboy.go
@@ -0,0 +1,9 @@
+package snowboydetect
+
+/*
+#cgo CXXFLAGS: -std=c++11
+#cgo linux,amd64 LDFLAGS: -lcblas -L${SRCDIR}/../../lib/ubuntu64 -lsnowboy-detect
+#cgo linux,arm   LDFLAGS: -lcblas -L${SRCDIR}/../../lib/rpi -lsnowboy-detect
+#cgo darwin      LDFLAGS: -lcblas -L${SRCDIR}/../../lib/osx -lsnowboy-detect
+ */
+import "C"
