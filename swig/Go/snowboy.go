b70a11f0fb160ef6db8888b91ab555d5a9fe50fa
Guillaume Saint-Raymond <guillaume.saintraymond@snips.ai>
2017-03-20 18:28:21 +0100

    Fix go bindinds compilation on archlinux

--- swig/Go/snowboy.go
+++ swig/Go/snowboy.go
@@ -1,7 +1,7 @@
 package snowboydetect
 
 /*
-#cgo CXXFLAGS: -std=c++11
+#cgo CXXFLAGS: -std=c++11 -D_GLIBCXX_USE_CXX11_ABI=0 
 #cgo linux,amd64 LDFLAGS: -lcblas -L${SRCDIR}/../../lib/ubuntu64 -lsnowboy-detect
 #cgo linux,arm   LDFLAGS: -lcblas -L${SRCDIR}/../../lib/rpi -lsnowboy-detect
 #cgo darwin      LDFLAGS: -lcblas -L${SRCDIR}/../../lib/osx -lsnowboy-detect
