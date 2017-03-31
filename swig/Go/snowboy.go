50931020dc6a651e6d643507930ac6c22e3adc32
Brent Dimmig <brentdimmig@gmail.com>
2017-03-30 22:19:27 -0400

    fix link order in go bindings for gcc/g++
    -lcblas must come after -lsnowboy-detect so the cblas
    symbols can be linked properly. Previously worked
    with with clang but not for gcc/g++.

--- swig/Go/snowboy.go
+++ swig/Go/snowboy.go
@@ -2,8 +2,8 @@ package snowboydetect
 
 /*
 #cgo CXXFLAGS: -std=c++11 -D_GLIBCXX_USE_CXX11_ABI=0 
-#cgo linux,amd64 LDFLAGS: -lcblas -L${SRCDIR}/../../lib/ubuntu64 -lsnowboy-detect
-#cgo linux,arm   LDFLAGS: -lcblas -L${SRCDIR}/../../lib/rpi -lsnowboy-detect
-#cgo darwin      LDFLAGS: -lcblas -L${SRCDIR}/../../lib/osx -lsnowboy-detect
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../../lib/ubuntu64 -lsnowboy-detect -lcblas
+#cgo linux,arm   LDFLAGS: -L${SRCDIR}/../../lib/rpi -lsnowboy-detect -lcblas
+#cgo darwin      LDFLAGS: -L${SRCDIR}/../../lib/osx -lsnowboy-detect -lcblas
  */
 import "C"
