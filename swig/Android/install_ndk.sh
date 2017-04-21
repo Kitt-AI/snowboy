5db0ce9a97f90e5703dac6db6f5e37126ab211c4
xuchen <yaoxuchen@gmail.com>
2017-04-21 14:41:37 -0700

    added android aarch64

--- swig/Android/install_ndk.sh
+++ swig/Android/install_ndk.sh
@@ -6,22 +6,28 @@
 UNAME_INFO=`uname -a`
 NDK_REPOSITORY_URL="https://dl.google.com/android/repository/"
 NDK_VERSION=$1
+BIT=$2
 
 if [[ $UNAME_INFO == *"Darwin"* ]]; then
   if [ ! -d "android-ndk-${NDK_VERSION}" ]; then
     wget -T 10 -t 3 ${NDK_REPOSITORY_URL}/android-ndk-${NDK_VERSION}-darwin-x86_64.zip \
       -O android-ndk-${NDK_VERSION}-darwin-x86_64.zip || exit 1;
+    unzip android-ndk-${NDK_VERSION}-darwin-x86_64.zip 1>/dev/null || exit 1;
   fi
-  unzip android-ndk-${NDK_VERSION}-darwin-x86_64.zip 1>/dev/null || exit 1;
 elif [[ $UNAME_INFO == *"Linux"* ]]; then
   if [ ! -d "android-ndk-${NDK_VERSION}" ]; then
     wget -T 10 -t 3 ${NDK_REPOSITORY_URL}/android-ndk-${NDK_VERSION}-linux-x86_64.zip \
       -O android-ndk-${NDK_VERSION}-linux-x86_64.zip || exit 1;
+    unzip android-ndk-${NDK_VERSION}-linux-x86_64.zip 1>/dev/null || exit 1;
   fi
-  unzip android-ndk-${NDK_VERSION}-linux-x86_64.zip 1>/dev/null || exit 1;
 else
   echo "Your platform is not supported yet." || exit 1;
 fi
 
-./android-ndk-${NDK_VERSION}/build/tools/make-standalone-toolchain.sh \
-  --arch=arm --platform=android-14 --install-dir=`pwd`/ndk_install || exit 1;
+if [[ $BIT == *"64"* ]]; then
+    ./android-ndk-${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --verbose \
+      --arch=arm64 --platform=android-21 --install-dir=`pwd`/ndk_install_64bit || exit 1;
+else
+    ./android-ndk-${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --verbose \
+      --arch=arm --platform=android-14 --install-dir=`pwd`/ndk_install_32bit || exit 1;
+fi
