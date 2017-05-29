ccd0157b2eab7871fbd7578fdc471f25891e6302
xuchen <yaoxuchen@gmail.com>
2017-05-29 12:44:31 -0700

    updated portaudio to support 64bit and newer mac os

--- examples/C/install_portaudio.sh
+++ examples/C/install_portaudio.sh
@@ -9,22 +9,19 @@
 
 echo "Installing portaudio"
 
-if [ ! -e pa_stable_v19_20140130.tgz ]; then
+if [ ! -e pa_stable_v190600_20161030.tgz ]; then
   wget -T 10 -t 3 \
-    http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz || exit 1;
+    http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz || exit 1;
 fi
 
-tar -xovzf pa_stable_v19_20140130.tgz || exit 1
+tar -xovzf pa_stable_v190600_20161030.tgz || exit 1
 
 cd portaudio
-patch < ../patches/portaudio.patch
 
 MACOS=`uname 2>/dev/null | grep Darwin`
 if [ -z "$MACOS" ]; then
   ./configure --without-jack --without-oss \
     --with-alsa --prefix=`pwd`/install --with-pic || exit 1;
-  sed -i '40s:src/common/pa_ringbuffer.o::g' Makefile
-  sed -i '40s:$: src/common/pa_ringbuffer.o:' Makefile
 else
   # People may have changed OSX's default configuration -- we use clang++.
   CC=clang CXX=clang++ ./configure --prefix=`pwd`/install --with-pic
