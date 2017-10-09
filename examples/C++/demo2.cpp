27c6e148c1f38bcb6417756cc5505e6da7eca9d5
jopedroliveira <j.pedrodiasoliveira@gmail.com>
2017-10-09 12:22:18 +0100

    styling fixes

--- examples/C++/demo2.cpp
+++ examples/C++/demo2.cpp
@@ -7,20 +7,19 @@
 #define model_filename "resources/snowboy.umdl" 
 #define sensitivity_str "0.5"
 
-struct wavHeader { //Para HEADER DE 44 bytes,
-  char  RIFF[4];  //4: 'RIFF', "low endian"
-  int RIFFsize; //4: size do "RIFF chunk" (não usuar)
-  char  fmt[8]; //8: string com 'WAVEfmt '
-  int fmtSize;  //4: size do "format chunk": usualmente 14, 16, 18 ou 40 bytes
-  short fmtTag; //2: "format tag". Apenas se considera 1=PCM (sem codificação)
-  short nchan;    //2: nº de canais (apenas mono ou stereo neste caso; isto é, não se considera WAVEFORMATEXTENSIBLE)
-  int fs;   //4: frequência de amostragem
-  int avgBps; //4: AvgBytesPerSec
-  short nBlockAlign;//2: nchan*bytes_per_value = bytes per sample;  
-  short  bps;   //2: Number of bits per sample of mono data (apenas presente se WAVEFORMATEX: fmtSize>=16)
-            //short   extraSize;  //2: The count in bytes of the extra size (nem sempre presente). 0 if fmtSize=18; 22 if fmtTag=65534
-  char  data[4];  //4: 'data' chunk
-  int datasize; //4: Num. Data Bytes (size of "data chunk")
+struct wavHeader { //44 byte HEADER only
+  char  RIFF[4];
+  int RIFFsize; 
+  char  fmt[8]; 
+  int fmtSize;  
+  short fmtTag; 
+  short nchan;  
+  int fs;   
+  int avgBps;
+  short nBlockAlign;
+  short  bps;   
+  char  data[4];
+  int datasize; 
 };
 
 
@@ -33,7 +32,7 @@ void readWavHeader(wavHeader *wavhdr, FILE *fi){
 // Returns a pointer pointing to the begining of the data
 
   char *tag = (char *)wavhdr;
-  fread(wavhdr, 34, 1, fi); //tag inicial tem de ser "RIFF"
+  fread(wavhdr, 34, 1, fi); //starting tag should be "RIFF"
   if (tag[0] != 'R' || tag[1] != 'I' || tag[2] != 'F' || tag[3] != 'F'){
     fclose(fi);
     perror("NO 'RIFF'.");
